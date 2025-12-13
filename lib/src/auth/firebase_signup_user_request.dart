6c35f74965de8112c21d02028ddc7c96f393956b
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-12-13 15:22:26 -0300

    Add password validation to FirebaseSignupUserRequest

--- lib/src/auth/firebase_signup_user_request.dart
+++ lib/src/auth/firebase_signup_user_request.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_backend/src/auth/dtos/firebase_signup_dto.dart';
 import 'package:firebase_backend/src/auth/firebase_auth_request.dart';
+import 'package:firebase_backend/src/domain/error/dto_validation_error.dart';
 import 'package:firebase_backend/src/domain/error/firebase_auth_error.dart';
 import 'package:firebase_backend/src/domain/error/firebase_request_dto_validation_error.dart';
-
 
 /// Creates a Firebase user sign-up request.
 /// This class implements the [FirebaseAuthRequest] interface to handle user
@@ -13,6 +13,19 @@ class FirebaseSignupUserRequest
   @override
   Future<UserCredential> execute(FirebaseSignupDto dto) async {
     if (!dto.validate()) {
+      throw FirebaseRequestDtoValidationError(dto.validationErrors);
+    }
+
+    if (!(await FirebaseAuth.instance.validatePassword(
+      FirebaseAuth.instance,
+      dto.password!,
+    )).isValid) {
+      dto.validationErrors.add(
+        DtoValidationError(
+          field: 'password',
+          validationError: 'Senha não atende aos requisitos de segurança.',
+        ),
+      );
       throw FirebaseRequestDtoValidationError(dto.validationErrors);
     }
 
