f45f80710a3745059354221c872711b7a0246276
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-12-13 15:12:21 -0300

    Refactor Firebase backend initialization documentation and ensure proper formatting

--- lib/firebase_backend.dart
+++ lib/firebase_backend.dart
@@ -3,6 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+
+/// Initializes the Firebase backend with the provided options.
+/// If [appCheck] is true, Firebase App Check is also initialized using
+/// the provided [recaptchaSiteKey] for web platforms.
 Future<void> intiFirebaseBackend({
   required FirebaseOptions options,
   bool appCheck = false,
@@ -15,7 +19,7 @@ Future<void> intiFirebaseBackend({
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: options);
+  await Firebase.initializeApp(options: options); 
 
   if (appCheck) {
     if (kIsWeb) {
