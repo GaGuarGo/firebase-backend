7ca2b7d47070fb985a69166598fc3015fb843c12
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-12-17 02:25:47 -0300

    feat: add FirebaseSignoutUserRequest class for user sign-out functionality

--- lib/src/auth/firebase_signout_user_request.dart
+++ lib/src/auth/firebase_signout_user_request.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_backend/src/auth/firebase_auth_request.dart';
 import 'package:firebase_backend/src/data/dto/firebase_request_dto.dart';
 
-class FirebaseSignoutRequest implements FirebaseAuthRequest {
+class FirebaseSignoutUserRequest implements FirebaseAuthRequest {
   @override
   Future execute(FirebaseRequestDto dto) async {
     await FirebaseAuth.instance.signOut();
