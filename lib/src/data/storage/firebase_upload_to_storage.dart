c5c888409911ed875fc65792a0177ec7783cdcb3
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-12-24 01:31:26 -0300

    docs: enhance documentation for FirebaseUploadToStorage class methods

--- lib/src/data/storage/firebase_upload_to_storage.dart
+++ lib/src/data/storage/firebase_upload_to_storage.dart
@@ -4,8 +4,18 @@ import 'package:firebase_backend/src/domain/error/firebase_storage_error.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 abstract class FirebaseUploadToStorage {
+  /// The Firestore collection path for the endpoint.
+  /// [path] is used to specify the collection from which documents will be retrieved.
+  /// For example, if your collection is named "users", the path would be "users".
+  /// If your collection is nested, you can specify the full path like "users/{userId}/posts".
   String get path;
 
+
+  /// Uploads a file to Firebase Storage.
+  /// [file] is the file to be uploaded. It can be a Blob (for web) or a File (for mobile).
+  /// [fileName] is an optional name for the file. If not provided, the original file name will be used.
+  /// [referenceBuilder] is an optional function to customize the storage reference.
+  /// Returns a Future that resolves to the download URL of the uploaded file.
   Future<String> upload({
     required dynamic file,
     String? fileName,
