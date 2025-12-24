1695b2a0cccc0e66eaa7288cf4bcfb6135a1df7f
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-12-24 01:29:54 -0300

    feat: implement FirebaseUploadStorage class and FirebaseStorageError exception handling

---
+++ lib/src/data/storage/firebase_upload_storage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import 'package:firebase_backend/src/domain/error/firebase_storage_error.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+abstract class FirebaseUploadStorage {
+  String get path;
+
+  Future<String> upload({
+    required dynamic file,
+    String? fileName,
+    Reference Function(Reference ref)? referenceBuilder,
+  }) async {
+    try {
+      final baseRef = FirebaseStorage.instance.ref(path);
+      final ref = referenceBuilder != null
+          ? referenceBuilder(baseRef)
+          : baseRef.child(
+              fileName ??
+                  (kIsWeb ? (fileName ?? 'file') : file.uri.pathSegments.last),
+            );
+
+      late TaskSnapshot uploadTask;
+
+      if (kIsWeb) {
+        uploadTask = await ref.putBlob(file);
+      } else {
+        uploadTask = await ref.putFile(file);
+      }
+
+      return await uploadTask.ref.getDownloadURL();
+    } catch (e) {
+      throw FirebaseStorageError('Failed to upload file to $path: $e');
+    }
+  }
+}
